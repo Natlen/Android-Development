30846ba6b2ce64546120717046d7a5f5032133c6
Nativ Lengel <nativlengel11@gmail.com>
2020-11-09 10:39:00 +0200

    Layout & Intents Essential

---
+++ 01-Layout and Intents/app/src/main/java/com/natlen/ex1/rl_t.java
@@ -0,0 +1,17 @@
+package com.natlen.ex1;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+public class rl_t extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rl_t);
+    }
+}
